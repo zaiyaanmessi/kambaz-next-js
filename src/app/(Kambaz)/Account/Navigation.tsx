7d42c1b4d8efb2b10760b2704445c90811f477eb
zaiyaan najam <zaiyaannajam@Zaiyaans-MacBook-Air.local>
2025-10-15 23:00:33 -0400

    final details

--- src/app/(Kambaz)/Account/Navigation.tsx
+++ src/app/(Kambaz)/Account/Navigation.tsx
@@ -1,9 +1,33 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+
 export default function AccountNavigation() {
- return (
-   <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-     <Link href="Signin" id="wd-account-signin-link" className="list-group-item text-danger active border-0"> Signin </Link> <br />
-     <Link href="Signup" id="wd-account-signup-link" className="list-group-item text-danger border-0"> Signup </Link> <br />
-     <Link href="Profile" id="wd-account-profile-link" className="list-group-item text-danger border-0"> Profile </Link> <br />
-   </div>
-);}
+  const pathname = usePathname();
+  const links = [
+    { label: "Signin", path: "/Account/Signin" },
+    { label: "Signup", path: "/Account/Signup" },
+    { label: "Profile", path: "/Account/Profile" }
+  ];
+
+  return (
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      {links.map((link) => (
+        <div key={link.label}>
+          <Link
+            href={link.path}
+            id={`wd-account-${link.label.toLowerCase()}-link`}
+            className={`list-group-item border-0 ${
+              pathname.includes(link.label)
+                ? "text-black border-start border-dark border-3"
+                : "text-danger"
+            }`}
+          >
+            {link.label}
+          </Link>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+}
